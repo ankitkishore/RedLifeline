d8f0d993bf60d74bd273e01a4a163d287a9988d1
Ankit Kishore <ankitkishore_aw@srmuniv.edu.in>
2018-06-16 13:17:33 +0530

    Combined divyam and Sriparna codes.

--- app/src/main/java/com/yef/redlifeline/about_us.java
+++ app/src/main/java/com/yef/redlifeline/about_us.java
@@ -3,11 +3,11 @@ package com.yef.redlifeline;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class about_us extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_about_us);
     }
 }
