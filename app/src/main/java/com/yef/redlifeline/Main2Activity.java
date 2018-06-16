69e918afa631da9adf239bef63565bb55aecff80
Sriparna101 <32588321+Sriparna101@users.noreply.github.com>
2018-06-16 15:24:46 +0530

    changes made to the xml file

--- app/src/main/java/com/yef/redlifeline/Main2Activity.java
+++ app/src/main/java/com/yef/redlifeline/Main2Activity.java
@@ -8,6 +8,6 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main21);
+        setContentView(R.layout.activity_100free);
     }
 }
