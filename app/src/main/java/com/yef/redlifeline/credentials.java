47aea9ecb24ba9770ca20d9aec29833e99940497
Ankit Kishore <ankitkishore_aw@srmuniv.edu.in>
2018-06-17 12:03:18 +0530

    Changes in XML files

--- app/src/main/java/com/yef/redlifeline/credentials.java
+++ app/src/main/java/com/yef/redlifeline/credentials.java
@@ -8,6 +8,6 @@ public class credentials extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main1);
+        setContentView(R.layout.credentials);
     }
 }
