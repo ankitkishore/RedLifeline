93e11876cfce5995a90d004ffcf6450b7da697fb
Sriparna101 <32588321+Sriparna101@users.noreply.github.com>
2018-06-26 19:10:47 +0530

    request blood page

---
+++ app/src/main/java/com/yef/redlifeline/request_blood.java
@@ -0,0 +1,13 @@
+package com.yef.redlifeline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class request_blood extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_blood);
+    }
+}
